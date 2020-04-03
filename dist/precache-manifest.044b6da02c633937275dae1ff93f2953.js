1a385f917809f418666fbbbe535116a26b1566b6
FionaKnight <62546233+FionaKnight@users.noreply.github.com>
2020-04-03 15:41:07 +0100

    Add files via upload

---
+++ dist/precache-manifest.044b6da02c633937275dae1ff93f2953.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428d90dde8a5d5af42fa25751a8ef2e0",
+    "url": "/blog.json"
+  },
+  {
+    "revision": "3cd05fdf036422102e30",
+    "url": "/css/about.4e62c4c9.css"
+  },
+  {
+    "revision": "c31df828a7ed1f314dae",
+    "url": "/css/app.64d688db.css"
+  },
+  {
+    "revision": "9653d4d04c0b1f99b63f",
+    "url": "/css/blog.01e050d0.css"
+  },
+  {
+    "revision": "8a4fb772dceb7eb99223",
+    "url": "/css/login.089cd2e3.css"
+  },
+  {
+    "revision": "71a101b8eeb14883f418",
+    "url": "/css/news.01e050d0.css"
+  },
+  {
+    "revision": "698b39e9e22fc57871e0",
+    "url": "/css/signup.1f60bb07.css"
+  },
+  {
+    "revision": "a1437e1135636615ce150adf3787a691",
+    "url": "/img/appimages/Cat-Face.png"
+  },
+  {
+    "revision": "bafe907d7829e13f3d5e227e79e0cadd",
+    "url": "/img/appimages/Cat-Face.webp"
+  },
+  {
+    "revision": "f9adbee404b71c0d3bbfda9b85b48924",
+    "url": "/img/appimages/Cat-Face_1x.png"
+  },
+  {
+    "revision": "6cb0f8d997c760ddc219d9213e6c46a4",
+    "url": "/img/appimages/Cat-Face_1x.webp"
+  },
+  {
+    "revision": "2319ce7d3953f4b0c6b35c352f1f9614",
+    "url": "/img/appimages/Cat_Sleeping.png"
+  },
+  {
+    "revision": "c5a3bebc8426954c48137209b8d4f84b",
+    "url": "/img/appimages/Cat_Sleeping.webp"
+  },
+  {
+    "revision": "f50fd286d75b2e26bb98a8b2f8dc19d2",
+    "url": "/img/appimages/Cat_Sleeping_1x.png"
+  },
+  {
+    "revision": "ead164e19e57b0b5f1080d206411d928",
+    "url": "/img/appimages/Cat_Sleeping_1x.webp"
+  },
+  {
+    "revision": "d6834877130141036db6e5337159e510",
+    "url": "/img/appimages/about.svg"
+  },
+  {
+    "revision": "90837826b2a7d7f915cfe8396a1d0f41",
+    "url": "/img/appimages/blog.svg"
+  },
+  {
+    "revision": "5d773d658694ea44d7de9a2912898445",
+    "url": "/img/appimages/cat-sitting.png"
+  },
+  {
+    "revision": "5b3bcbd83d4267d1e1d5cfe3b9d45add",
+    "url": "/img/appimages/cat-sitting.webp"
+  },
+  {
+    "revision": "b4da13f985378588e4902630b06f1bd5",
+    "url": "/img/appimages/cat-sitting_1x.png"
+  },
+  {
+    "revision": "d5738eb7836c4c708d92adb3c5af4bc2",
+    "url": "/img/appimages/cat-sitting_1x.webp"
+  },
+  {
+    "revision": "6fe3da6e341f774cde08ed1b951172f9",
+    "url": "/img/appimages/catheader.jpg"
+  },
+  {
+    "revision": "353388e4d51956eff0812ffd858acc7f",
+    "url": "/img/appimages/catheader.webp"
+  },
+  {
+    "revision": "2a1c5320a502eb04ad2128a8460d6a8a",
+    "url": "/img/appimages/catheader2.jpg"
+  },
+  {
+    "revision": "085a32b30604e7327c3dbfc574d2b669",
+    "url": "/img/appimages/catheader2.webp"
+  },
+  {
+    "revision": "bc253aefb711beaff9cd970dfa38fb50",
+    "url": "/img/appimages/catheader2_small.jpg"
+  },
+  {
+    "revision": "30396fbdd265f82e592179ae031ce304",
+    "url": "/img/appimages/catheader2_small.webp"
+  },
+  {
+    "revision": "e263890add4747a386f31e989f0c6950",
+    "url": "/img/appimages/catheader3.jpg"
+  },
+  {
+    "revision": "39ed0156ff68f388f2be5c204a4e687b",
+    "url": "/img/appimages/catheader3.webp"
+  },
+  {
+    "revision": "04af8a724eedbffc69ae81b2d5308fe3",
+    "url": "/img/appimages/catheader3_small.jpg"
+  },
+  {
+    "revision": "0ca3a88830dd9164714c60acedb327a2",
+    "url": "/img/appimages/catheader3_small.webp"
+  },
+  {
+    "revision": "809707e5c39ab8a705cc2cba05c1b6a0",
+    "url": "/img/appimages/catheader4.jpg"
+  },
+  {
+    "revision": "f63fcc0079344b5bd845ead9d2244bac",
+    "url": "/img/appimages/catheader4.webp"
+  },
+  {
+    "revision": "d46640731fd8095f0ca20a09a0243a57",
+    "url": "/img/appimages/catheader4_small.jpg"
+  },
+  {
+    "revision": "735c4455f1959318688b07ae81a5a9d0",
+    "url": "/img/appimages/catheader4_small.webp"
+  },
+  {
+    "revision": "f60e89ab55717c5c8c59eed2fc7b3868",
+    "url": "/img/appimages/catheader_small.jpg"
+  },
+  {
+    "revision": "657b9d56a730546ccb31ee1b24934609",
+    "url": "/img/appimages/catheader_small.webp"
+  },
+  {
+    "revision": "f3b7bc03f64b1edd6a355af4fc6a6d5f",
+    "url": "/img/appimages/catstand.png"
+  },
+  {
+    "revision": "f19563bb8c2c72880575d1518209a7af",
+    "url": "/img/appimages/catstand.webp"
+  },
+  {
+    "revision": "37c2c9796514d0b2ca555cfbff3718de",
+    "url": "/img/appimages/catstand_1x.png"
+  },
+  {
+    "revision": "e1bb1efdd2044e87c0ca86079e89dc64",
+    "url": "/img/appimages/catstand_1x.webp"
+  },
+  {
+    "revision": "85c89525d1095d2e902cf1c180c2cc38",
+    "url": "/img/appimages/dino.jpg"
+  },
+  {
+    "revision": "d8c3a7965ad037c5c224f5de17d8e5ad",
+    "url": "/img/appimages/home.svg"
+  },
+  {
+    "revision": "00044beb517ad2af57e0a45e95f11088",
+    "url": "/img/appimages/news.svg"
+  },
+  {
+    "revision": "da57fec6e5a098a4db518ff403d5349b",
+    "url": "/img/appimages/offlinemap.jpg"
+  },
+  {
+    "revision": "6411c699e0cfe52ec3c6198326d282e0",
+    "url": "/img/blogimages/cat1.jpg"
+  },
+  {
+    "revision": "0fa192c30bbc63be18c51ed72a7d02b7",
+    "url": "/img/blogimages/cat2.jpg"
+  },
+  {
+    "revision": "9f869cf12324b3ed49e233a85e4cbbe7",
+    "url": "/img/blogimages/cat3.jpg"
+  },
+  {
+    "revision": "015b5983b8b5c94d671ffd5a927bc0c3",
+    "url": "/img/blogimages/cat4.jpg"
+  },
+  {
+    "revision": "a40cb8b885bebf3dce17291e1f625f0c",
+    "url": "/img/blogimages/cat5.jpg"
+  },
+  {
+    "revision": "c5bae4e1310edbf1637ba5a92d202249",
+    "url": "/img/blogimages/cat6.jpg"
+  },
+  {
+    "revision": "4c4140038adbd42967a13014fb3f9d31",
+    "url": "/img/fonts/Raleway-Regular.eot"
+  },
+  {
+    "revision": "f4b782bda9e0d7eb6ba140ca9d9c155d",
+    "url": "/img/fonts/Raleway-Regular.ttf"
+  },
+  {
+    "revision": "145cf3f6f532d9b61cfefcd1e6d3f1ff",
+    "url": "/img/fonts/Raleway-Regular.woff"
+  },
+  {
+    "revision": "3f13c93fa79af5552240264d03cb2556",
+    "url": "/img/fonts/Raleway-Regular.woff2"
+  },
+  {
+    "revision": "05b78c7912745ab71e8013a4bda3eadf",
+    "url": "/img/fonts/Roboto-Regular.eot"
+  },
+  {
+    "revision": "2f0037fec3f315f2f5ff217cf29f0d6a",
+    "url": "/img/fonts/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "ede8a1b839e55efb78e568f851b4e3a0",
+    "url": "/img/fonts/Roboto-Regular.woff"
+  },
+  {
+    "revision": "b5fd381a4a1501508c91f0e6a45a4319",
+    "url": "/img/fonts/Roboto-Regular.woff2"
+  },
+  {
+    "revision": "47f048fbd79adce75e93e7af399e7366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd05fdf036422102e30",
+    "url": "/js/about.a7e82230.js"
+  },
+  {
+    "revision": "c31df828a7ed1f314dae",
+    "url": "/js/app.48d12491.js"
+  },
+  {
+    "revision": "9653d4d04c0b1f99b63f",
+    "url": "/js/blog.ad8166f5.js"
+  },
+  {
+    "revision": "a3537eab42a9e9c25f71",
+    "url": "/js/chunk-vendors.d8615c60.js"
+  },
+  {
+    "revision": "8a4fb772dceb7eb99223",
+    "url": "/js/login.bdcaa941.js"
+  },
+  {
+    "revision": "3aaeaa7afb804f418728",
+    "url": "/js/mycats.2d9f1b57.js"
+  },
+  {
+    "revision": "71a101b8eeb14883f418",
+    "url": "/js/news.3e358483.js"
+  },
+  {
+    "revision": "698b39e9e22fc57871e0",
+    "url": "/js/signup.fe518d4a.js"
+  },
+  {
+    "revision": "e95c9180a2279b091ebbf08ef6b430de",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "112272e51c80ffe5bd01becd2ce7d656",
+    "url": "/normalize.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4e6013085be47233311eed5c627c45cf",
+    "url": "/service-worker.js"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,33 @@
+importScripts("/precache-manifest.044b6da02c633937275dae1ff93f2953.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-console */
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded`);
+    workbox.setConfig({
+        debug: false,
+    });
+    workbox.precaching.precacheAndRoute([]);
+    workbox.routing.registerNavigationRoute('/index.html');
+    workbox.routing.registerRoute(
+        /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                }),
+            ],
+        })
+    );
+    workbox.routing.registerRoute(
+        /\.(?:js|css)$/,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'static-resources',
+        })
+    );
+} else {
+    console.log(`Boo! Workbox didn't load`);
+}
+
